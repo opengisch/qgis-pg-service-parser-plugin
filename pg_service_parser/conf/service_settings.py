41e5c56333952e09870618a10229135da81a1e6f
Germán Carrillo <gcarrillo@linuxmail.org>
2024-05-16 15:17:31 -0500

    Allow users to create a service file (with a simple template) at a default location if it's not found when the dialog is opened

--- pg_service_parser/conf/service_settings.py
+++ pg_service_parser/conf/service_settings.py
@@ -1,9 +1,17 @@
+# Settings available for manual addition
 # See https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-PARAMKEYWORDS
 SERVICE_SETTINGS = {
     "host": "localhost",
     "port": "5432",
-    "dbname": "",
+    "dbname": "test",
     "user": "",
     "password": "",
     "passfile": "",
+}
+
+# Settings to initialize new files
+SETTINGS_TEMPLATE = {
+    "host": "localhost",
+    "port": "5432",
+    "dbname": "test",
 }
